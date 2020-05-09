add3d1e63644f7071e1f4361b31eee764f9896f1
Павел <shmachilin@gmail.com>
2020-05-10 01:08:36 +0300

    Потери

--- Tests/ConsoleTest/FDTD1D.cs
+++ Tests/ConsoleTest/FDTD1D.cs
@@ -36,14 +36,20 @@ namespace ConsoleTest
 
         private static readonly double[] __Hy = new double[__Size];
         private static readonly double[] __Ez = new double[__Size];
-        private static readonly double[] __Eps = Enumerable
-           .Range(0, __Size)
-           .Select(i => i < 100 ? 1d : 9d)
-           .ToArray(); 
-        private static readonly double[] __Mu = Enumerable
-           .Range(0, __Size)
-           .Select(i => 1d)
-           .ToArray();
+        private static readonly double[] __Eps = new Array<double>.Creator(__Size)
+           .Init(i => i < 100 ? 1d : 9d)
+           .Create();
+        private static readonly double[] __Mu = new Array<double>.Creator(__Size)
+           .Init(i => 1d)
+           .Create();
+
+        private const double __Loss = 0.005;
+        private static readonly double[] __ELossE = new Array<double>.Creator(__Size)
+                .Init(i => i < 100 ? 1 : (1 - __Loss) / (1 + __Loss))
+                .Create();
+        private static readonly double[] __ELossH = new Array<double>.Creator(__Size)
+                .Init(i => i < 100 ? 1 : 1 / (1 + __Loss))
+                .Create();
 
         public static void Start()
         {
@@ -79,7 +85,8 @@ namespace ConsoleTest
         private static void ProcessEz()
         {
             for (var i = 1; i < __Size; i++)
-                __Ez[i] += (__Hy[i] - __Hy[i - 1]) * __Impl0 / __Eps[i];
+                __Ez[i] = __ELossE[i] * (__Ez[i])
+                        + __ELossH[i] * (__Hy[i] - __Hy[i - 1]) * __Impl0 / __Eps[i];
         }
 
         private static void SourceHy(double t) { }
