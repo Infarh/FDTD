176d4e95ea7e86ca979dfc68566c8726766f43e6
Павел <shmachilin@gmail.com>
2020-05-09 23:39:20 +0300

    Начало...

---
+++ Tests/ConsoleTest/FDTD1D.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.IO;
+
+namespace ConsoleTest
+{
+    internal static class FDTD1D
+    {
+        internal class DataWriter : IDisposable
+        {
+            private readonly string _Format;
+            private readonly StreamWriter _Writer;
+
+            public DataWriter(string FileName, string Format = "f3")
+            {
+                _Format = Format;
+                _Writer = new StreamWriter(FileName, false);
+            }
+
+            public void Write(params double[] Data)
+            {
+                var data = Data.Select(v => v.ToString(_Format, CultureInfo.InvariantCulture));
+                var str = string.Join(",", data);
+                _Writer.WriteLine(str);
+                Console.WriteLine(str);
+            }
+
+            public void Dispose() => _Writer?.Dispose();
+        }
+
+        private const double __Impl0 = 377;
+        private const int __Size = 200;
+
+        private static readonly double[] __Hy = new double[__Size];
+        private static readonly double[] __Ez = new double[__Size];
+
+        public static void Start()
+        {
+            using var writer = new DataWriter("data.csv");
+            const int max_time = 250;
+            for (var t = 0; t < max_time; t++)
+            {
+                ProcessHy();
+                ProcessEz();
+
+                __Ez[50] += F0(t, t0: 30, tau: 100);
+
+                if(t % 10 == 0)
+                    writer.Write(__Ez);
+            }
+        }
+
+        private static double F0(double t, double t0, double tau)
+        {
+            var t1 = t - t0;
+            return Math.Exp(-t1 * t1 / tau);
+        }
+
+        private static void ProcessHy()
+        {
+            for (var i = 0; i < __Size - 1; i++)
+                __Hy[i] += (__Ez[i + 1] - __Ez[i]) / __Impl0;
+        }
+
+        private static void ProcessEz()
+        {
+            for (var i = 1; i < __Size; i++)
+                __Ez[i] += (__Hy[i] - __Hy[i - 1]) * __Impl0;
+        }
+    }
+}
---
+++ Tests/ConsoleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FDTD1D.Start();
+
+            Console.WriteLine("Copmleted...");
+            Console.ReadLine();
+        }
+    }
+}
